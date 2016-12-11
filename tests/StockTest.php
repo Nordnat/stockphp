97afccca40d97d87c0c5d32ec0fc15a6a8f244d2
Nordraf <rkamieniarz@gmail.com>
2016-12-11 11:29:31 +0100

    method overload - fix

--- tests/StockTest.php
+++ tests/StockTest.php
@@ -10,6 +10,7 @@ class StockTest extends TestCase
     {
         $this->stock = new Stock();
     }
+
     public function testAdd()
     {
         $element = 'silnik';
@@ -28,6 +29,7 @@ class StockTest extends TestCase
         $this->assertEquals(3, count($stock_array));
 
     }
+
     public function testTake()
     {
         $element = 'silnik';
@@ -41,5 +43,20 @@ class StockTest extends TestCase
         $stock_array = $property->getValue($this->stock);
         $this->assertEquals(0, count($stock_array));
         $this->assertEquals($taken_element, $element);
+    }
+
+    public function testAddMethodIfAcceptingArrays()
+    {
+        $element1 = ['brakes', 'wheel'];
+        $this->stock->add($element1);
+
+        $class = new ReflectionClass($this->stock);
+
+        $property = $class->getProperty('stock');
+        $property->setAccessible(true);
+        $stock_array = $property->getValue($this->stock);
+        $this->assertEquals($stock_array[0], 'brakes');
+        $this->assertEquals($stock_array[1], 'wheel');
+        $this->assertEquals(2, count($stock_array));
     }
 }
