2100eb09bcc7fdfe62f83c6dd75e7ee4ec46d200
Nordnat <rkamieniarz@gmail.com>
2017-02-05 18:45:09 +0100

    tests fix

--- tests/GoodsTest.php
+++ tests/GoodsTest.php
@@ -4,58 +4,67 @@ use Stock\Models\Goods;
 
 class GoodsTest extends TestCase
 {
-    public $goods;
+    public $goods_data = [];
 
     public function setUp()
     {
-        $this->goods = new Goods(['name' => 'brakes', 'price' => 199.00, 'producer' => 'acme', 'quantity' => 20]);
+        $this->goods_data = ['name' => 'brakes', 'price' => 199.00, 'producer' => 'acme', 'quantity' => 20];
     }
+
 
     public function testCheckTypeMethod()
     {
-        $class = new ReflectionClass($this->goods);
+        $goods = new Goods($this->goods_data);
+        
+        $class = new ReflectionClass($goods);
         $method = $class->getMethod('check_type');
         $method->setAccessible(true);
 
-        $this->assertTrue($method->invokeArgs($this->goods, ['silnik', 'string']));
-        $this->assertTrue($method->invokeArgs($this->goods, [124, 'integer']));
-        $this->assertTrue($method->invokeArgs($this->goods, [199.00, 'double']));
-        $this->assertFalse($method->invokeArgs($this->goods, [20, 'string']));
-        $this->assertFalse($method->invokeArgs($this->goods, [199, 'double']));
+        $this->assertTrue($method->invokeArgs($goods, ['silnik', 'string']));
+        $this->assertTrue($method->invokeArgs($goods, [124, 'integer']));
+        $this->assertTrue($method->invokeArgs($goods, [199.00, 'double']));
+        $this->assertFalse($method->invokeArgs($goods, [20, 'string']));
+        $this->assertFalse($method->invokeArgs($goods, [199, 'double']));
     }
 
     public function testCheckRequiredMethod()
     {
-        $class = new ReflectionClass($this->goods);
+        $goods = new Goods($this->goods_data);
+        
+        $class = new ReflectionClass($goods);
         $method = $class->getMethod('check_required');
         $method->setAccessible(true);
 
-        $this->assertTrue($method->invokeArgs($this->goods, ['silnik', true]));
-        $this->assertTrue($method->invokeArgs($this->goods, [ null, false]));
-        $this->assertFalse($method->invokeArgs($this->goods, [ null, true]));
+        $this->assertTrue($method->invokeArgs($goods, ['silnik', true]));
+        $this->assertTrue($method->invokeArgs($goods, [ null, false]));
+        $this->assertFalse($method->invokeArgs($goods, [ null, true]));
     }
 
     public function testGoodsValidationMethod()
     {
+        $goods = new Goods($this->goods_data);
+        
         $good_proper = ['name' => 'brakes', 'price' => 199.00, 'producer' => 'acme', 'quantity' => 20];
         $good_wrong_type = ['name' => 'brakes', 'price' => '199.00', 'producer' => 'acme', 'quantity' => 20];
         $good_lack_required = ['name' => null, 'price' => 199.00, 'producer' => 'acme', 'quantity' => 20];
 
-        $class = new ReflectionClass($this->goods);
+        $class = new ReflectionClass($goods);
         $method = $class->getMethod('goodsValidation');
         $method->setAccessible(true);
 
-        $this->assertTrue($method->invokeArgs($this->goods, [$good_proper, true]));
-        $this->assertFalse($method->invokeArgs($this->goods, [$good_wrong_type, false]));
-        $this->assertFalse($method->invokeArgs($this->goods, [$good_lack_required, false]));
+        $this->assertTrue($method->invokeArgs($goods, [$good_proper, true]));
+        $this->assertFalse($method->invokeArgs($goods, [$good_wrong_type, false]));
+        $this->assertFalse($method->invokeArgs($goods, [$good_lack_required, false]));
     }
 
     public function testGoodsSerialCodeGenerator()
     {
-        $class = new ReflectionClass($this->goods);
+        $goods = new Goods($this->goods_data);
+
+        $class = new ReflectionClass($goods);
         $method = $class->getMethod('serialCodeGenerator');
         $method->setAccessible(true);
-        $serialCode = $method->invokeArgs($this->goods, [$this->goods]);
+        $serialCode = $method->invokeArgs($goods, [$goods]);
         $this->assertRegExp('/[a-zA-Z]{3}[0-9]{13}/', $serialCode);
     }
 }
